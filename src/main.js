ca9214f77f707660c9afacaefba70c0c4ba94d98
MysticalYcc <15155137510@163.com>
2022-05-09 21:16:27 +0800

    组件数据传播

--- src/main.js
+++ src/main.js
@@ -4,8 +4,13 @@ import ElementPlus from 'element-plus';
 import 'element-plus/theme-chalk/index.css';
 import router from "@/routes";
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import Emitter from 'tiny-emitter'
+
+const emitter = new Emitter()
 
 const app = createApp(App).use(router).use(ElementPlus)
+// 绑定为全局可用
+app.config.globalProperties.emitter = emitter
 app.mount('#app')
 for (const [key, value] of Object.entries(ElementPlusIconsVue)) {
     app.component(key, value)
